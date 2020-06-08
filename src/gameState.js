796e06f372062baa25b91b1304484618ad6c3aa9
Suyash Chandra <suyash.chandra24@gmail.com>
2020-06-08 11:30:43 +0530

    Making the buttons responsive :hammer:

--- src/gameState.js
+++ src/gameState.js
@@ -1,14 +1,55 @@
 const gameState = {
   current: "INIT",
+  wakeTime: -1,
   clock: 1,
   tick() {
     this.clock++;
     console.log("clock", this.clock);
     return this.clock;
   },
+  startGame() {
+    console.log("hatching");
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+  },
+  wake() {
+    console.log("awoken");
+    this.current = "IDLING";
+    this.wakeTime = -1;
+  },
   handleUserAction(icon) {
-    console.log(icon);
+    if (
+      ["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
+    ) {
+      return;
+    }
+    if (this.current === "INIT" || this.current === "DEAD") {
+      this.startGame();
+      return;
+    }
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+    }
+  },
+
+  changeWeather() {
+    console.log("Weather");
+  },
+  cleanUpPoop() {
+    console.log("Clean");
+  },
+  feed() {
+    console.log("food");
   },
 };
 
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
 export default gameState;
